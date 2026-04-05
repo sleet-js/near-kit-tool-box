0739802ad570bf92604ad84a00f748f8203317bb
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-05 17:31:09 -0500

    Update pool_fun.ts

--- packages/fun/src/fun/pool/pool_fun.ts
+++ packages/fun/src/fun/pool/pool_fun.ts
@@ -3,11 +3,22 @@ import { pool_methods_const } from "@sleet-js/staking-pool-contract-methods-cons
 import type { FinalExecutionOutcome } from "near-kit";
 // ===========================================
 // view
-// 
+// get_number_of_accounts
 // call
 // ping
 // ===========================================
-export async function ping(
+export async function pool_get_number_of_accounts_fun(
+  near: Near,
+  pool_contractId: string,
+) {
+  const result = await near.view(
+    pool_contractId,
+    pool_methods_const.get_number_of_accounts,
+  );
+  return result as number;
+}
+// ===========================================
+export async function pool_ping_fun(
   near: Near,
   pool_contractId: string,
 ) {
