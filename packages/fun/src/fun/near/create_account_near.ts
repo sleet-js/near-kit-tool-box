d4dd0c69b9b6df675035027e5cf54aee4922c184
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-06 17:54:38 -0500

    re

--- packages/fun/src/fun/near/create_account_near.ts
+++ packages/fun/src/fun/near/create_account_near.ts
@@ -5,12 +5,12 @@ const method_create_account = "create_account";
 // ===========================================
 export async function near_create_account_near_fun(
   near: Near,
-  near_contractId: string,
+  near_tld_contractId: string,
   new_account_id: string,
   new_public_key: string,
 ) {
   const result = await near.call(
-    near_contractId,
+    near_tld_contractId,
     method_create_account,
     { new_account_id: new_account_id, new_public_key: new_public_key },
     { gas: "30 Tgas" },
