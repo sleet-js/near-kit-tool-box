32571a70be4ed6d7cc4c1f194da568153147b194
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-06-03 12:06:18 -0500

    Update pumpopoly_world_fun.ts

--- packages/fun/src/fun/pumpopoly/pumpopoly_world_fun.ts
+++ packages/fun/src/fun/pumpopoly/pumpopoly_world_fun.ts
@@ -4,6 +4,7 @@ import type { FinalExecutionOutcome } from "near-kit";
 // ===========================================
 // call
 // move_player
+// elite_move
 // ===========================================
 // pumpopoly_move_player_fun
 export async function pumpopoly_move_player_fun(
@@ -15,6 +16,20 @@ export async function pumpopoly_move_player_fun(
     world_pumpopoly_methods_const.move_player,
     {},
     { gas: "60 Tgas" },
+  );
+  return result as FinalExecutionOutcome;
+}
+// ===========================================
+// pumpopoly_elite_move_fun
+export async function pumpopoly_elite_move_fun(
+  near: Near,
+  pumpopoly_contractId: string,
+) {
+  const result = await near.call(
+    pumpopoly_contractId,
+    world_pumpopoly_methods_const.elite_move,
+    { contract_id: "1" },
+    { gas: "100 Tgas" },
   );
   return result as FinalExecutionOutcome;
 }
